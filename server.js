22d60da02afd5416ba6cca62677a854e6f022e91
Bohdan Strilets <bohdan.strilets@gmail.com>
2024-05-24 12:54:36 +0200

    Created server with first controller - Auth. Added schema for user.

---
+++ server.js
@@ -0,0 +1,27 @@
+import 'colors'
+import dotenv from 'dotenv'
+import express from 'express'
+import morgan from 'morgan'
+import authRoutes from './app/auth/auth.routes.js'
+
+dotenv.config()
+
+const app = express()
+
+async function main() {
+	if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+
+	app.use(express.json())
+	app.use('/api/auth', authRoutes)
+
+	const PORT = process.env.PORT || 5000
+
+	app.listen(
+		PORT,
+		console.log(
+			`🚀 Server running in ${process.env.NODE_ENV} mode on ${PORT}`.blue.bold
+		)
+	)
+}
+
+main()
